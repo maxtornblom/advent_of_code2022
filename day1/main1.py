06ab7c145c91cf437edc1560ffc32f522ef2dff3
Max Törnblom <90630609+maxtornblom@users.noreply.github.com>
2022-12-02 22:47:30 +0100

    cleanup

--- day1/main1.py
+++ day1/main1.py
@@ -1,18 +1,15 @@
 file = open("day1/input.txt", "r")
 
-
-
 maxi = 0
 current = 0
 for x in file.readlines():
-    if x != '\n':
+    if x != "\n":
         current += int(x)
 
     else:
         if current > maxi:
             maxi = current
-        
+
         current = 0
 
 print(maxi)
-
--- day1/main2.py
+++ day1/main2.py
@@ -4,16 +4,12 @@ input_array = []
 
 current = 0
 for x in file.readlines():
-    if x != '\n':
+    if x != "\n":
         current += int(x)
         input_array.append(current)
 
     else:
         current = 0
 
-
 input_array.sort(reverse=True)
 print(sum(input_array[:3]))
-
-
-
