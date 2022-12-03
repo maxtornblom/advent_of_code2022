b3968f58c75ca0ba864355759b732f97674794f1
Max Törnblom <90630609+maxtornblom@users.noreply.github.com>
2022-12-03 14:42:31 +0100

    Update part2.py

--- day1/part2.py
+++ day1/part2.py
@@ -2,14 +2,14 @@ file = open("day1/input.txt", "r")
 
 input_array = []
 
-current = 0
+current_cal = 0
 for x in file.readlines():
     if x != "\n":
-        current += int(x)
+        current_cal += int(x)
         input_array.append(current)
 
     else:
-        current = 0
+        current_cal = 0
 
 input_array.sort(reverse=True)
 print(sum(input_array[:3]))
